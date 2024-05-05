f14935cf02c5d0d9451a7da2d7264fe0527c2a8d
GalangRamadhan1 <150521341+GalangRamadhan1@users.noreply.github.com>
2024-05-05 19:58:52 +0700

    Upload UTS

---
+++ src/constans/index.tsx
@@ -0,0 +1,20 @@
+export const apiKey = "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";
+
+export const dummyMessages = [
+    {
+        role: 'user', 
+        content: 'Apa kabar?'
+    },
+    {
+        role: 'assistant',
+        content: "Saya baik-baik saja, ada yang bisa saya bantu hari ini."
+    },
+    {
+        role: 'user',
+        content: 'Siapa pembuat aplikasi ini?'
+    },
+    {
+        role: 'assistant',
+        Text: "Saya adalah Galang Ramadhan seorang sofware developer. Saya pembuat aplikasi ini."
+    }
+]
